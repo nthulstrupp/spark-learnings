16b6fbd22aaee29e730fe4971b09f8e90e3f9d03
nthulstrup <nikolaj.thulstrup@trivago.com>
2019-01-13 14:02:33 +0100

    add new code

--- pyspark/workflow/workflow.py
+++ pyspark/workflow/workflow.py
@@ -1,6 +1,17 @@
-def workflow(spark_context: SQLContext, crunch_datetime: datetime, use_full_data: bool, algo_version: str):
+def workflow(spark_context: SQLContext, crunch_datetime: datetime):
 
     print("start {}".format(datetime.now()))
 
     config = CrunchConfiguration(
         crunch_datetime=crunch_datetime)
+        
+    read_data = read_data(spark_context, config)
+    
+    agg_data = agg_data(read_data)
+    
+    insert_overwrite_with_repartition(data_frame=agg_data,
+                                      db_name=your_db,
+                                      table_name='your_table',
+                                      new_partitions=1)
+                                      
+    print('Workflow ended at: {}'.format(datetime.now()))
